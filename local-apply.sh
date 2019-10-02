caa654b4eedd1649ad6ab09ab2dedbe6d8f88d92
Steven Riggs <steven.riggs@icloud.com>
2019-10-02 08:47:47 -0400

    Create local-apply.sh

---
+++ local-apply.sh
@@ -0,0 +1,61 @@
+# Copyright (c) 2019, UK HealthCare (https://ukhealthcare.uky.edu) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+##########################
+
+echo "ensure the correct environment is selected..."
+KUBECONTEXT=$(kubectl config view -o template --template='{{ index . "current-context" }}')
+if [ "$KUBECONTEXT" != "docker-desktop" ]; then
+	echo "ERROR: Script is running in the wrong Kubernetes Environment: $KUBECONTEXT"
+	exit 1
+else
+	echo "Verified Kubernetes context: $KUBECONTEXT"
+fi
+
+##########################
+
+echo "setup the persistent volume for registry...."
+mkdir -p /Users/Shared/Kubernetes/persistent-volumes/default/registry
+kubectl apply -f ./kubernetes/registry-local-pv.yaml
+
+##########################
+
+echo "deploy registry...."
+kubectl apply -f ./kubernetes/registry.yaml
+
+echo "wait for registry..."
+sleep 2
+isPodReady=""
+isPodReadyCount=0
+until [ "$isPodReady" == "true" ]
+do
+	isPodReady=$(kubectl get pod -l app=registry -o jsonpath="{.items[0].status.containerStatuses[*].ready}")
+	if [ "$isPodReady" != "true" ]; then
+		((isPodReadyCount++))
+		if [ "$isPodReadyCount" -gt "100" ]; then
+			echo "ERROR: timeout waiting for registry pod. Exit script!"
+			exit 1
+		else
+			echo "waiting...registry pod is not ready...($isPodReadyCount/100)"
+			sleep 2
+		fi
+	fi
+done
+
+##########################
+
+kubectl get pods
+
+echo "...done"
