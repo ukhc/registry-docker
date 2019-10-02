74c1f12f4561f719c4b0e172038983833d79dd57
Steven Riggs <steven.riggs@icloud.com>
2019-10-02 08:55:53 -0400

    Create local-delete.sh

---
+++ local-delete.sh
@@ -0,0 +1,40 @@
+# Copyright (c) 2019, UK HealthCare (https://ukhealthcare.uky.edu) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+##########################
+
+echo "ensure the correct environment is selected..."
+KUBECONTEXT=$(kubectl config view -o template --template='{{ index . "current-context" }}')
+if [ "$KUBECONTEXT" != "docker-desktop" ]; then
+	echo "ERROR: Script is running in the wrong Kubernetes Environment: $KUBECONTEXT"
+	exit 1
+else
+	echo "Verified Kubernetes context: $KUBECONTEXT"
+fi
+
+##########################
+
+echo "delete registry...."
+kubectl delete -f ./kubernetes/registry.yaml
+
+##########################
+
+echo "delete the persistent volume for registry...."
+kubectl delete -f ./kubernetes/registry-local-pv.yaml
+rm -rf /Users/Shared/Kubernetes/persistent-volumes/default/registry
+
+##########################
+
+echo "...done"
